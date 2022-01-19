8405c461bcd37082bb67a23cf03bb9bad104947d
Dongwan Kim <dongwan_kim@tmax.co.kr>
2022-01-19 17:43:52 +0900

    example code for xi2

---
+++ xlib/xi2/test.c
@@ -0,0 +1,105 @@
+/* 
+ * list.c - list touch devices and their properties
+ *
+ * Copyright © 2013 Eon S. Jeon <esjeon@live.com>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the “Software”), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ * 
+ */
+
+#include <stdio.h>
+#include <X11/extensions/XInput2.h>
+
+
+char* type2str(int type){
+      switch(type)
+      {
+        case XITouchClass:
+          return "XITouchClass";
+        case XIButtonClass:
+          return "XIButtonClass";
+        case XIScrollClass:
+          return "XIScrollClass";
+
+        case XIValuatorClass:
+          return "XIValuatorClass";
+        case XIKeyClass:
+          return "XIKeyClass";
+        default:
+          return "Unknown";
+
+
+      }
+
+      return "";
+}
+int
+main ()
+{
+	Display *dpy = XOpenDisplay(NULL);
+
+	XIDeviceInfo *di;
+	XIDeviceInfo *dev;
+	XITouchClassInfo *class;
+	int cnt;
+	int printed;
+	int i, j;
+
+	puts("");
+
+	di = XIQueryDevice(dpy, XIAllDevices, &cnt);
+	for (i = 0; i < cnt; i ++)
+	{
+		dev = &di[i];
+
+		printed = 0;
+		for (j = 0; j < dev->num_classes; j ++)
+		{
+			class = (XITouchClassInfo*)(dev->classes[j]);
+
+
+      printf("####################\n");
+      printf("device name : %s\n", dev->name);
+      printf("device id : %d\n", dev->deviceid);
+      printf("use  : %d\n", dev->use);
+      printf("attachment  : %d\n", dev->attachment);
+      printf("enabled  : %d\n", dev->enabled);
+      printf("num_classes  : %d\n", dev->num_classes);
+
+      printf("type  : %x => %s\n", class->type , type2str(class->type));
+
+			if (class->type != XITouchClass)
+				continue;
+
+			if (!printed)
+				printf("Device %d '%s'\n", dev->deviceid, dev->name),
+				printed = 1;
+
+	 		printf("\t%s touch\n\t%d touches\n",
+				(class->mode == XIDirectTouch) ?  "DIRECT" : "DEPENDENT",
+				class->num_touches
+			);
+		}
+	}
+	puts("");
+
+
+	return 0;
+}
+
